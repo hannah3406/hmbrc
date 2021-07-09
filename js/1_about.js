ab69a2e40616e26992918f69eeae320a195da232
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-07-09 18:35:09 +0900

    20210709

--- js/1_about.js
+++ js/1_about.js
@@ -1,4 +1,12 @@
 
+//toggle
+const elBurgur = document.querySelector('.menu-trigger');
+const elBg = document.querySelector('.menu_bg');
+	elBurgur.addEventListener('click',function(){
+		this.classList.toggle('active-7');
+		elBg.classList.toggle('active');
+      });
+
 
 //pop1
 //팝업 열리게
