7de467e6601af9276505dac571384895a014be12
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-07-13 12:24:49 +0900

    20210713

--- js/common.js
+++ js/common.js
@@ -2,10 +2,12 @@
 const elBurgur = document.querySelector('.menu-trigger');
 const elBg = document.querySelector('.menu_bg');
 const elBox = document.querySelector('.menu_box');
-
+const elBackgroundMenu = document.querySelector('.background-menu');
+console.log(elBackgroundMenu);
 	elBurgur.addEventListener('click',function(){
 		this.classList.toggle('active-7');
 		elBg.classList.toggle('active');
 		elBox.classList.toggle('active1');
+		elBackgroundMenu.classList.toggle('active2');
 });
 
