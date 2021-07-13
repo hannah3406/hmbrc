f5d1933bc3c943402832c933b91f1d97598f9c4a
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-07-13 11:58:20 +0900

    20210713

---
+++ js/2_rescue.js
@@ -0,0 +1,47 @@
+//pop-1 관련법안 상세보기
+   //열기
+btnPopup1.addEventListener('click',function(){
+    elPopup1.classList.add('open1');
+})
+
+   //닫기
+elPopup1.addEventListener('click',function(){
+    if(event.target.nodeName !='IMG'){
+        elPopup1.classList.remove('open1');}
+})
+
+//pop-2
+   //열기
+    btnPopup2.addEventListener('click',function(){
+    elPopup2.classList.add('open2');
+})
+
+   //닫기
+elPopup2.addEventListener('click',function(){
+    if(event.target.nodeName =='A'){
+        elPopup2.classList.remove('open2');}
+})
+
+//pop-3
+   //열기
+   btnPopup3.addEventListener('click',function(){
+    elPopup3.classList.add('open3');
+})
+
+   //닫기
+elPopup3.addEventListener('click',function(){
+    if(event.target.nodeName =='A'){
+        elPopup3.classList.remove('open3');}
+})
+
+//pop-4
+   //열기
+   btnPopup4.addEventListener('click',function(){
+    elPopup4.classList.add('open4');
+})
+
+   //닫기
+elPopup4.addEventListener('click',function(){
+    if(event.target.nodeName =='A'){
+        elPopup4.classList.remove('open4');}
+})
