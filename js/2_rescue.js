23b84d53f28f87e46946a8b69724b63afa64ff2d
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-07-14 18:23:43 +0900

    20210714

--- js/2_rescue.js
+++ js/2_rescue.js
@@ -45,3 +45,20 @@ elPopup4.addEventListener('click',function(){
     if(event.target.nodeName =='A'){
         elPopup4.classList.remove('open4');}
 })
+
+
+//scroll event
+
+//스크롤 내리면 글 올라오며 등장
+window.addEventListener('scroll',function(){
+    const elM = document.querySelectorAll('.elM');
+    
+    for(let i=0;i<elM.length;i++){
+            let elHei = elM[i].offsetTop;
+            let winHei = window.innerHeight;
+            
+        if(elHei-winHei <= window.scrollY){
+            elM[i].classList.add('active');
+        }
+    }
+})
