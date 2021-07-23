c448bc43b6a4d6538640e93c22af2cc4780d27d6
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-07-23 11:40:26 +0900

    20210723

--- js/4_research.js
+++ js/4_research.js
@@ -1,6 +1,6 @@
 //scroll event
 
-//스크롤 내리면 글 올라오며 등장
+//스크롤 내리면 이미지와 글 양쪽에서 슬라이드
 window.addEventListener('scroll',function(){
     const elM = document.querySelectorAll('.elM');
     
@@ -13,3 +13,19 @@ window.addEventListener('scroll',function(){
         }
     }
 })
+
+//tab 기능
+const elTabBtn = document.querySelectorAll('.sub4_31 a');
+const elTabCon = document.querySelectorAll('.tabBox');
+let k = 0;
+
+    for(let i=0;i<elTabBtn.length;i++){            
+        elTabBtn[i].addEventListener('click',function(){
+            elTabBtn[k].classList.remove('active');
+            elTabCon[k].classList.remove('active');
+
+            elTabBtn[i].classList.add('active');
+            elTabCon[i].classList.add('active');
+            k = i;
+        });
+    }
