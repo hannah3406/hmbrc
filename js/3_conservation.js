f958e616ce11091002d92020f5024070013dc1ae
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-07-23 17:46:19 +0900

    Update 3_conservation.js

--- js/3_conservation.js
+++ js/3_conservation.js
@@ -100,6 +100,35 @@ fetch('js/data.json')
     });
     };
 
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+    
+
 }
 
 
